0aad036075e4cb6e3d58aa8961c8c510f2fc0a02
Carlos Eduardo Turina <146773453+anirutsolrak@users.noreply.github.com>
2024-08-01 20:23:40 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Container } from '@mui/material'; // Importe o Container
+import { Container, Typography, Button, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
+// Importações dos componentes
 import Header from './componentes/Cabecalho/Header';
 import Rodape from './componentes/Rodape/Rodape';
 import Home from './Paginas/Home';
 import DetalhesLivro from './Paginas/PaginaDetalhesLivro';
 import Perfil from './Paginas/Perfil';
 import Carrinho from './Paginas/Carrinho';
+import PaginaNaoEncontrada from './Paginas/PaginaNaoEncontrada';
 
 const queryClient = new QueryClient();
 
@@ -17,14 +20,13 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Container className="App">
-          {' '}
-     {/* Usando Container do MUI */}
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/livro/:id" element={<DetalhesLivro />} />
             <Route path="/perfil" element={<Perfil />} />
             <Route path="/carrinho" element={<Carrinho />} />
+            <Route path="/*" element={<PaginaNaoEncontrada />} />
           </Routes>
           <Rodape />
         </Container>
